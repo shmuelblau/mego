d62b89f61a54cc6aaaf93e1196712656d6445d72
shmuelblau <a0528075188@gmail.com>
2024-04-10 01:49:39 +0300

    incloding printer end sam check

--- python/projects/shesh_besh/shesh_besh/shesh_besh.py
+++ python/projects/shesh_besh/shesh_besh/shesh_besh.py
@@ -1,5 +1,43 @@
-import random
-
+﻿import random
+def Printer(board):
+    print('\n    ','▬'*41)
+    for i in range(1,9):
+        print('    ▌', end = '')
+        for j in range(12,0,-1):
+            piece = board[j]
+            if(piece > 0 and piece>=i):
+                print(' ● ', end = '')
+            elif(piece < 0 and -piece >= i):
+                print(' ○ ', end = '')
+            else:
+                print('   ', end = '')
+            if j == 7:
+                print('▐', end = '')
+                if -board[0] >= i:
+                    print(' ○ ▌', end = '')
+                else:
+                    print('   ▌', end = '')
+        print('▐')
+    i = j = 0
+    for i in range(6,0,-1):
+        print('    ▌', end = '')
+        for j in range(13,25):
+            piece = board[j]
+            if(piece > 0 and piece >= i):
+                print(' ● ', end = '')
+            elif(piece < 0 and -piece >= i):
+                print(' ○ ', end = '')
+            else:
+                print('   ', end = '')
+            if j == 18:
+                print('▐', end = '')
+                if board[25] >= i:
+                    print(' ● ▌', end = '')
+                else:
+                    print('   ▌', end = '')
+        print('▐')
+    print("")
+ 
 def check_won(board):
     
     for i in range(26):
@@ -55,6 +93,7 @@ def transfer(board,player,dice,loc):
     if player%2 ==0:
         if dice+loc > 24:
             board[loc]-=1
+            
         else:
             if board[loc+dice] == -1:
                 board[loc+dice]+=1
@@ -63,7 +102,7 @@ def transfer(board,player,dice,loc):
             board[loc]-=1
             board[loc+dice]+=1
     else:
-        if dice-loc <1:
+        if loc-dice < 1:
             board[loc]+=1
         else:
             if board[loc-dice] == 1:
@@ -74,10 +113,11 @@ def transfer(board,player,dice,loc):
             board[loc-dice]-=1
            
     return board
-board=[0,2,0,0,0,0,-5,0,-3,0,0,0,5,-5,0,0,0,3,0,5,0,0,0,0,-2,0]
+board=[2,2,0,0,0,0,-5,0,-3,0,0,0,5,-5,0,0,0,3,0,5,0,0,0,0,-2,-1]
 player=0
 dice=[0,0]
 choices=[1,1,0,0]
+Printer(board)
 while True:
     
     dice=[0,0]
@@ -88,7 +128,19 @@ while True:
         choices[2]=1
         choices[3]=1
         dice=[dice[0]]*4
-    print("Your dice are: {} {}".format(dice[0],dice[1]))
+    print("player: {}   Your dice are: {} {}".format(player%2,dice[0],dice[1]))
+    
+    while True:
+        if board[(player%2)*25] != 0 :
+             choiceply=int(input("you have a game pieces out enter namber dice to play"))
+             if check(board,player,dice[choiceply],(player%2)*25) == True:
+                board=transfer(board,player,dice[choiceply],(player%2)*25)
+                choices[choiceply]= 0
+             else:
+                 print(check(board,player,dice[choiceply],(player%2)*25))
+        else:
+            break
+        
            
     
     while True:    
@@ -99,18 +151,20 @@ while True:
         
         while True:  
             
-            choiceply=int(input("Type the number of the die you want to play with"))
+            choiceply=int(input("Type the number of the dice you want to play with"))
             if (choiceply >-1 and choiceply<5) and choices[choiceply]!= 0 :
-                
                 break
             
-        loc=int(input("Where do you want to insert the number {}".format(dice[choiceply])))  
+        loc=int(input("Where do you want to insert the number {}".format(dice[choiceply])))
         if check(board,player,dice[choiceply],loc) == True:
             board=transfer(board,player,dice[choiceply],loc)
             choices[choiceply]= 0
+        else:
+            print(check(board,player,dice[choiceply],loc))
         if sum(choices)==0:
             break
-    print(board)    
+       
+    Printer(board)
     if check_won == False:
         print("player namber {} won!!!".format(player%2))
         break
